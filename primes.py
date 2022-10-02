f04d4482a6b0abc24ffcaf70f49d75fae1cd77b6
SeoHyun Kwon <seohyun.kwon@kcl.ac.uk>
2022-10-02 23:06:38 +0100

    Fix file structure

--- primes.py
+++ primes.py
@@ -2,5 +2,25 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    list = [2]
+
+    curNum=3
+
+    while len(list)<number_of_primes:
+        isPrime=True
+        print('curNum', curNum)
+        divider=2
+        while(divider<curNum):
+            print('divider', divider)
+            if curNum%divider==0:
+                isPrime=False
+            divider=divider+1
+        
+        if isPrime:
+            list.append(curNum)
+
+        curNum=curNum+1
+
     return list
+
+print(primes(10))
